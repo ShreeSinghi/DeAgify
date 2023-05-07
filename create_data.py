55c25d6e13babbcad1b9326d307ba1dcb3100645
ShreeSinghi <singhishree@gmail.com>
2023-05-07 14:37:37 +0530

    Adds all files

--- create_data.py
+++ create_data.py
@@ -5,18 +5,18 @@ import random
 from joblib import load, dump
 from matplotlib import pyplot as plt
 
-MEAN, STD = 120., 64.
+shape = 128
 AGE_MEAN, AGE_STD = 35.3, 15.54
 
-tf.keras.backend.set_floatx('float32')
-divs = 5
+divs = 2
 
 files = os.listdir('UTKFace')
 # file name is [age]_[gender]_[race]_[date&time].jpg
 
-# we set a lower and upper bound for age
-files = list(filter(lambda x:6<int(x.split('_')[0])<80, files))
+# we set a lower bound for age
+files = list(filter(lambda x:6<int(x.split('_')[0]), files))
 ages = np.array([int(x.split('_')[0]) for x in files])
+genders = np.array([int(x.split('_')[1]) for x in files])
 
 random.shuffle(files)
 
@@ -27,17 +27,21 @@ for i in range(divs):
     if i == divs-1:
         file_set = files[i*set_size:]
         
-    ages = np.array([int(x.split('_')[0]) for x in file_set])
-    ages = (ages-AGE_MEAN)/AGE_STD 
+    ages = (np.array([int(x.split('_')[0]) for x in file_set], dtype='float32')-AGE_MEAN)/AGE_STD
+    genders = np.array([int(x.split('_')[1]) for x in file_set])
     
-    images = np.zeros((len(file_set),120,120,3), dtype='float32')
+    images = np.zeros((len(file_set), shape, shape, 3), dtype='float32')
 
     for j, file in enumerate(file_set):
-        images[j] = tf.image.resize(tf.keras.utils.img_to_array(tf.keras.utils.load_img(f"UTKFace/{file}")), (120,120))
+        images[j] = tf.image.resize(tf.keras.utils.img_to_array(tf.keras.utils.load_img(f"UTKFace/{file}")), (shape, shape))
 
-    images = (images-MEAN)/STD
+    images = (images-127.5)/127.5
     dump(images, f'data/{i}_images.joblib')
     dump(ages, f'data/{i}_ages.joblib')
+    dump(genders, f'data/{i}_genders.joblib')
     
     plt.hist(ages, bins=30)
     plt.show()
+    plt.hist(genders)
+    plt.show()
+
